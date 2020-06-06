c23244e1c219ab59f33f81c7dd5a109d385bb101
Jose Gomes <zemiguelgomes99@gmail.com>
2020-06-06 21:27:53 +0100

    Added Beats, Attack and Realese

---
+++ waveViewer.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+import matplotlib.pyplot as plot
+
+df = pd.read_csv('output.csv',header=None)
+print(df.head())
+df.shape
+plot.figure(figsize=(20,10))
+plot.plot(df[22000:24000])
+plot.show()
+plot.figure(figsize=(20,10))
+plot.plot(df[0:24000])
+plot.show()
